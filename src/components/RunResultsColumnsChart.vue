3e344ebe74124293f310204b095bb8830fbb5de1
adamwardecki <adam.wardecki@gmail.com>
2022-11-02 17:07:35 +0100

    2 files modified

--- src/components/RunResultsColumnsChart.vue
+++ src/components/RunResultsColumnsChart.vue
@@ -3,71 +3,66 @@
     <div class="text-left">
       <h3 class="text-xl font-semibold">Check Run Results</h3>
       <p>
-        Stacked column chart with run results segmented by success, failure, and degraded.
+        Stacked column chart with run results segmented by success, failure, and
+        degraded.
       </p>
     </div>
-    <highcharts
-      :options="defaultOptions"
-    ></highcharts>
+    <highcharts :options="defaultOptions"></highcharts>
   </div>
 </template>
 
 <script setup>
 import { computed } from 'vue';
 import { mergeDates } from '../fixtures/helpers.js';
-import {
-  dates,
-  failures,
-  degradations,
-  success
-} from '../fixtures/data.js';
+import { dates, failures, degradations, success } from '../fixtures/data.js';
 
 const defaultOptions = computed(() => ({
-    chart: {
-      marginTop: 34,
-    },
+  chart: {
+    marginTop: 34,
+    height: 200,
+  },
+  title: '',
+  yAxis: {
     title: '',
-    yAxis: {
-      title: '',
+  },
+  xAxis: {
+    type: 'datetime',
+  },
+  series: [
+    {
+      name: 'Failure',
+      type: 'column',
+      data: mergeDates(failures, dates),
+      color: '#BF0B23',
     },
-    xAxis: {
-      type: 'datetime',
-    },
-    series: [
-      {
-        name: 'Failure',
-        type: 'column',
-        data: mergeDates(failures, dates),
-        color: '#BF0B23',
-      },
-      {
-        name: 'Degraded',
-        type: 'column',
+    {
+      name: 'Degraded',
+      type: 'column',
 
-        data: mergeDates(degradations, dates),
-        color: '#F5A623',
+      data: mergeDates(degradations, dates),
+      color: '#F5A623',
+    },
+    {
+      name: 'Success',
+      type: 'column',
+      data: mergeDates(success, dates),
+      color: '#5ced73',
+    },
+  ],
+  legend: {
+    enabled: true,
+    align: 'left',
+  },
+  credits: {
+    enabled: false,
+  },
+  plotOptions: {
+    column: {
+      stacking: 'normal',
+      dataLabels: {
+        enabled: false,
       },
-      {
-        name: 'Success',
-        type: 'column',
-        data: mergeDates(success, dates),
-        color: '#333',
-      },
-    ],
-    legend: {
-      enabled: true,
-      align: 'left',
     },
-    credits: {
-      enabled: false,
-    },
-    plotOptions: {
-      column: {
-          stacking: 'normal',
-          dataLabels: {
-              enabled: false
-          }
-      }
-    },
-  }))
+  },
+}));
 </script>
