24c6c6b85041a6644fbb6cbb95c325460c48b1de
miliberlin <michelle.liebheit@posteo.de>
2022-11-11 17:33:35 +0100

    feat: add key listener to single chart

--- src/PrototypeSingleChart.vue
+++ src/PrototypeSingleChart.vue
@@ -25,7 +25,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { results, events, createdAtTimeStamps } from './fixtures/moreData'
 import Drawer from './components/Drawer.vue'
 import ResponseRunsChartWithDrawer from './components/ResponseRunsChartWithDrawer.vue'
@@ -43,4 +43,17 @@ function openDrawer () {
     isDrawerOpen.value = true
   }
 }
+
+function escapeHandler (event) {
+  if (event.key === 'Escape') {
+    event.preventDefault()
+    if (isDrawerOpen.value) {
+      isDrawerOpen.value = false
+    }
+  }
+}
+
+onMounted(() => {
+  document.addEventListener('keydown', escapeHandler)
+})
 </script>
